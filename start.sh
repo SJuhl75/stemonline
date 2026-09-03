788d7b8e7c4bc770d96c013a1bd193f8949d8714
Stefan Juhl <stefanjuhl75@gmail.com>
2026-09-03 12:17:42 +0000

    FIX

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "=== Konfiguriere MagentaCloud WebDAV Verbindung ==="
 
 # rclone Config-Ordner erstellen
-mkdir -p ~/.config/rclone/
+mkdir -p ~/.config/rclone/ 
 
 # Erstellt die verschlüsselte Verbindung on-the-fly beim Start des Pods
 cat <<EOF > ~/.config/rclone/rclone.conf
